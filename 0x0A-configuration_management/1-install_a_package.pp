a76074cefd0f6ca2a3281b53a7f9a93a700cf29a
Mawuawoe <desmonddzakago@gmail.com>
2024-07-12 11:08:24 +0000

    task_2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"',
-  require => Package['python3-pip'],
+#!/usr/bin/pup
+# Install specific version of flask (2.1.0)
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
