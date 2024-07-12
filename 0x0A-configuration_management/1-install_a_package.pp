4950bdb5d11ae3685b5f1f4d62af0986f58791bb
Mawuawoe <desmonddzakago@gmail.com>
2024-07-12 11:16:00 +0000

    task_1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,20 @@
 #!/usr/bin/pup
 # Install specific version of flask (2.1.0)
+# Ensure pip3 is installed
 package { 'python3-pip':
   ensure => 'installed',
 }
 
+# Install a specific version of Werkzeug compatible with Flask 2.1.0
+package { 'werkzeug':
+  ensure   => '2.0.3',  # or another compatible version
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+  require  => Package['werkzeug'],
 }
