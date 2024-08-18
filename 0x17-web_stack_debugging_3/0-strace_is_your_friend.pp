be50b56e288b976af0dbcba52856e53ea1ed7a6a
Mawuawoe <desmonddzakago@gmail.com>
2024-08-18 19:58:58 +0000

    task_1

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# script to solve the issue, correct the extention phpp to`wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
