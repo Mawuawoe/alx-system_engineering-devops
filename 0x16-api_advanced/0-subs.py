8b20db81ebac187bae4e11300ec141ff85490801
Mawuawoe <desmonddzakago@gmail.com>
2024-08-20 19:12:28 +0000

    task_0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,21 +19,19 @@ def number_of_subscribers(subreddit):
     # Custom User-Agent to avoid request limits imposed by Reddit
     headers = {"User-Agent": "subreddit-subscriber-counter/1.0"}
 
-    try:
-        # Sending a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
+ 
+    # Sending a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        # Check if the request was successful
-        if response.status_code == 200:
+    # Check if the request was successful
+    if response.status_code == 200:
 
-            # Parse the JSON response into a Python dictionary
-            data = response.json()
+        # Parse the JSON response into a Python dictionary
+        data = response.json()
 
-            # Return the number of subscribers
-            # (if present), otherwise return 0
-            return data['data'].get('subscribers')
-        else:
-            # If the status code is not 200, return 0
-            return 0
-    except requests.RequestException:
+        # Return the number of subscribers
+        # (if present), otherwise return 0
+        return data['data'].get('subscribers')
+    else:
+        # If the status code is not 200, return 0
         return 0
