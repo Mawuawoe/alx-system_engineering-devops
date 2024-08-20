74bf48239d5c94cc23fa3b47375f0d831db69b52
Mawuawoe <desmonddzakago@gmail.com>
2024-08-20 21:10:44 +0000

    task_1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,8 +8,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "subreddit-post-fetcher/1.0"}
-
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
